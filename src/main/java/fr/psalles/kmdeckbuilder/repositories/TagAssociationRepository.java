b3a5119992e6e919561d2dc41057b54e860f776a
314R-Salles <sallespierre@live.fr>
2025-07-13 16:13:53 +0200

    fix pour la détection du deck inchangé ne monte pas la version : besoin de delete manuellement les entitées filles parce que les entités sont pas propres ( embedded dans embedded + joins )
    La date de création d'une nouvelle version est la date de création du deck original

---
+++ src/main/java/fr/psalles/kmdeckbuilder/repositories/TagAssociationRepository.java
@@ -0,0 +1,18 @@
+package fr.psalles.kmdeckbuilder.repositories;
+
+import fr.psalles.kmdeckbuilder.models.entities.TagAssociation;
+import fr.psalles.kmdeckbuilder.models.entities.embedded.TagAssociationIdentity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TagAssociationRepository extends JpaRepository<TagAssociation, TagAssociationIdentity> {
+
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "delete from multiwork.km_tag_association ta WHERE ta.deckId = :deckId and ta.version = :version", nativeQuery = true)
+    void deleteByDeckEntity(String deckId, int version);
+
+}
